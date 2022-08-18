a503d1025fc2b6eaeea7ac8ce08040f9a7fa373e
jyoti <jyotidhage10@gmail.com>
2022-08-18 16:20:52 +0530

    [Aaaradhya]Add:Given Three Integers Find Maximum Number

---
+++ MaximumNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPrograms
+{
+    public class MaximumNumbers
+    {
+        public static int MaxIntNumber(int firstnumb, int secondnumb, int thirdnumb)
+        {
+            if (firstnumb.CompareTo(secondnumb) > 0 && firstnumb.CompareTo(thirdnumb) > 0)
+            {
+                return firstnumb;
+            }
+            else if (secondnumb.CompareTo(firstnumb) > 0 && secondnumb.CompareTo(thirdnumb) > 0)
+            {
+                return secondnumb;
+            }
+            else if (thirdnumb.CompareTo(firstnumb) > 0 && thirdnumb.CompareTo(secondnumb) > 0)
+            {
+                return thirdnumb;
+            }
+            else
+            {
+                Console.WriteLine("all 3 numbers are same");
+                return 0;
+            }
+        }
+    }
+
+}
+    
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Find MaximumProblem using Generics");
+            
             Console.WriteLine(MaximumNumbers.MaxIntNumber(786, 60, 212));
             Console.WriteLine(MaximumNumbers.MaxIntNumber(70, 733, 212));
             Console.WriteLine(MaximumNumbers.MaxIntNumber(943, 73, 3312));
