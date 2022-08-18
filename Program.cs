da0bc8e1b0009950ffa4e0ea9d194e2a895e84e9
jyoti <jyotidhage10@gmail.com>
2022-08-18 16:14:30 +0530

    find Maximum Number Using Generics

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericsPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Find MaximumProblem using Generics");
+            Console.WriteLine(MaximumNumbers.MaxIntNumber(786, 60, 212));
+            Console.WriteLine(MaximumNumbers.MaxIntNumber(70, 733, 212));
+            Console.WriteLine(MaximumNumbers.MaxIntNumber(943, 73, 3312));
+            Console.Read();
+        }
+    }
+}
